de9aa1838ffb909c5f7d61bf43ad38692d386325
Huan <hu.patatyy@gmail.com>
2024-05-29 20:54:19 -0300

    debugging em java

---
+++ controle-fluxo/src/br/com/dio/debugging/Main.java
@@ -0,0 +1,28 @@
+package br.com.dio.debugging;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Iniciou o programa");
+        a();
+        System.out.println("Finalizou o a porra toda");
+    }
+
+    static void a(){
+        System.out.println("Entrou no metodo a");
+        b();
+        System.out.println("Finalizou o metodo a");
+    }
+
+    static void b(){
+        System.out.println("Entrou no metodo b");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o metodo b");
+    }
+
+    static void c(){
+        System.out.println("Entrou no metodo c");
+        Thread.dumpStack();
+        System.out.println("Finalizou o metodo c");
+    }
+}
